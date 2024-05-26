32f489320c74fde1137bffe5754a34745dc566f4
lxj <2727809960@qq.com>
2024-05-26 16:31:02 +0800

    slot

---
+++ vuecli/vuecli_5_26/src/slot/App.vue
@@ -0,0 +1,59 @@
+<template>
+<div>
+    <!-- <showMsg title="标题行" content="内容区域"></showMsg> -->
+    <!-- <showMsg title="标题行">
+        <button>按钮</button>
+    </showMsg>
+
+    <showMsg title="标题行">
+        <a href="#">超链接跳转</a>
+    </showMsg>
+
+    <showMsg title="标题行">
+        <img src="../assets/logo.png" alt="">
+    </showMsg> -->
+    <!-- 插槽的默认内容 -->
+    <!-- <showMsg title="标题行">
+        
+    </showMsg> -->
+
+    <navBar>
+        <template v-slot:left>
+            <button>返回</button>
+        </template>
+        <template v-slot:center>
+            <span>内容</span>
+        </template>
+        <template v-slot:right>
+            <a href="#">登录</a>
+        </template>
+        
+    </navBar>
+
+    <navBar>
+        <!-- 语法糖 v-slot: == # -->
+        <template #left>
+            <button>返回</button>
+        </template>
+     
+    </navBar>
+    
+</div>
+  
+</template>
+
+<script>
+import showMsg from './components/showMsg.vue'
+import navBar from './components/navBar.vue'
+export default {
+    components: {
+        showMsg,
+        navBar
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vuecli/vuecli_5_26/src/slot/components/showMsg.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    <h2>{{ title }}</h2>
+    <div class="content">
+        <slot>
+            <p>插槽的默认内容</p>    
+        </slot>    
+    </div>   
+     
+</div>
+  
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: "默认值",
+        },
+        content: {
+            type: String,
+            default: "",
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
