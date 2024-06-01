c3a623dad694e87dd90d89e204b0e16bedd348ff
lxj <2727809960@qq.com>
2024-06-01 18:15:58 +0800

    24_6_1

--- vuecli/vuecli_5_26/src/main.js
+++ vuecli/vuecli_5_26/src/main.js
@@ -3,7 +3,12 @@ import { createApp } from 'vue'
 // import App from './slot/App.vue' 
 // import App from './provide_inject/App.vue'
 // import App from './eventBus/App.vue'
-import App from './ref/App.vue'
+// import App from './生命周期/App.vue'
+// import App from './ref/App.vue'
+import App from './动态组件/App.vue'
+// import App from './keep-alive/App.vue'
+
+
 
 
 
---
+++ vuecli/vuecli_5_26/src/动态组件/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+    <h2>Home组件</h2>
+    <h2>{{ name }} - {{ age }}</h2>
+    <button @click="homeBtnClick">btn</button>
+</div>
+  
+</template>
+
+<script>
+export default {
+    // keep-alive的include属性中的名字
+    name: 'home',
+    emits: ['homeClick'],
+    methods: {
+        homeBtnClick() {
+            this.$emit("homeClick","home")
+        }
+    },
+    created () {
+        console.log("home created")
+    },
+    // 销毁前
+    unmouted() {
+        console.log("home unmounted")
+    },
+    activated () {
+        console.log("activated")
+    },
+    deactivated () {
+        console.log("deactivated")
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
