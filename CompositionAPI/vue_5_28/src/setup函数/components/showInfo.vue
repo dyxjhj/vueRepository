74eaad859a2141263dfb8f1c0095498923c41a8b
lxj <2727809960@qq.com>
2024-06-05 23:44:20 +0800

    24_6_5

---
+++ CompositionAPI/vue_5_28/src/setup函数/components/showInfo.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <h2>{{ info }}</h2>
+    
+</div>
+  
+</template>
+
+<script>
+export default {
+    // 接收父组件传值
+    props: {
+        info: {
+            type: Object,
+            default: () => {}
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
